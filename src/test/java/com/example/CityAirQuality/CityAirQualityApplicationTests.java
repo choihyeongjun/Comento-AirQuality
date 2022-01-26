6a25f2c1b33831aca7bac95be80e9527cc84a316
choihyeongjun <chj5156@naver.com>
2022-01-26 17:10:56 +0900

    test

---
+++ src/test/java/com/example/CityAirQuality/CityAirQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CityAirQuality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityAirQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
